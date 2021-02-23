02b2701793ef0b76f90d4c6228b5a9df2b961825
THOMAS <thomas@rocketlab.com.br>
2021-02-23 07:42:50 -0300

    Dia 1 completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-
-import { useState } from 'react';
 import { ExperienceBar } from './components/ExperienceBar'
 import './styles/global.css'
 
 function App() {
-  const [currentXP, setCurrenXP] = useState(0);
-  function incrementa() {
-    if (currentXP <= 590) {
-      setCurrenXP(currentXP + 10)
-    }
-  }
-  function reset() {
-    setCurrenXP(0)
-  }
+  const currentXP = 80;
 
   return (
     <div className='container' >
       <ExperienceBar currentXP={currentXP} ></ExperienceBar>
-      <button className='button-xp' onClick={incrementa} >Incrementa XP</button>
-      <button className='button-xp' onClick={reset} >Zera XP</button>
     </div>
   );
 }
